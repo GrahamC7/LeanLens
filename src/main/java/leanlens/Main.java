c3efd389539c0f7d4b37ed6b7394692470c21ddf
Graham <scocker@wgu.edu>
2025-05-16 14:20:50 -0400

    creating ConsoleColor.java - enum class for color constants, refactored Main.java

--- src/main/java/leanlens/Main.java
+++ src/main/java/leanlens/Main.java
@@ -3,11 +3,6 @@ package leanlens;
 import java.util.Map;
 
 public class Main {
-  public static final String RESET = "\u001B[0m";
-  public static final String GREEN = "\u001B[32m";
-  public static final String YELLOW = "\u001B[33m";
-  public static final String BLUE = "\u001B[34m";
-  public static final String BOLD = "\u001B[1m";
 
   public static void main(String[] args) {
     // Step 1: Conduct survey
@@ -15,15 +10,17 @@ public class Main {
     String[] userResponses = surveyApp.conductSurvey();
 
     // Step 2: Show user a summary of their answers
-    System.out.println(BOLD + BLUE + "\nThanks for completing the survey! Here's a quick summary of your responses:\n" + RESET);
+    System.out.println(ConsoleColor.BOLD.code() + ConsoleColor.BLUE.code() +
+            "\nThanks for completing the survey! Here's a quick summary of your responses:\n" +
+            ConsoleColor.RESET.code());
 
     String[] questions = SurveyApp.getQuestions();
     for (int i = 0; i < questions.length - 1; i++) { // skip final question (party choice)
       String question = questions[i];
       String answer = SurveyApp.getAnswerText(userResponses[i]);
-      System.out.println(BOLD + (i + 1) + ". " + questions[i] + RESET);
-      System.out.println("   → Answer: " + YELLOW + SurveyApp.getAnswerText(userResponses[i]) + RESET + "\n");
 
+      System.out.println(ConsoleColor.BOLD.code() + (i + 1) + ". " + question + ConsoleColor.RESET.code());
+      System.out.println("   → Answer: " + ConsoleColor.YELLOW.code() + answer + ConsoleColor.RESET.code() + "\n");
     }
 
     // Step 3: Load training data
@@ -46,8 +43,9 @@ public class Main {
     NaiveBayesClassifier classifier = new NaiveBayesClassifier(partyDataMap);
     String predictedParty = classifier.predict(userResponses);
 
-    System.out.println(BOLD + "\nPredicting political leaning..." + RESET);
-    System.out.println(GREEN + "\nPredicted political party: " + predictedParty + RESET);
+    System.out.println(ConsoleColor.BOLD.code() + "\nPredicting political leaning..." + ConsoleColor.RESET.code());
+    System.out.println(ConsoleColor.GREEN.code() + "\nPredicted political party: " +
+            predictedParty + ConsoleColor.RESET.code());
   }
 }
 
